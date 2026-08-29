306f03d8f9bf2faa4bae66240fafd57be94c04b4
widney <widneylima21@gmail.com>
2026-08-29 17:20:08 -0300

    Format printer order

--- src/components/OrdersComponents/OrderDetails/OrderDetails.tsx
+++ src/components/OrdersComponents/OrderDetails/OrderDetails.tsx
@@ -23,6 +23,7 @@ import { type Order } from "../../../types/Order.type";
 import STATUS_COLOR from "../../../utils/colors/colors";
 import { STATUS, STATUS_LABEL } from "../../../utils/texts/status.enum";
 import { moneyMask, paymentMethodMask } from "../../../utils/masks/mask";
+import PrintOrder from "../../../utils/print/orderPrint";
 
 interface OrderDetailProps {
   order: Order | null;
@@ -30,14 +31,8 @@ interface OrderDetailProps {
   updateStatusOrder: (status: string, orderId: string) => void;
 }
 
-
-export default function OrderDetail({ 
-  order, 
-  onClose,
-  updateStatusOrder
-  }: OrderDetailProps) {
-
-  const {open, openModalOrderCancel, handleOpenModalOrderCancel, handleCloseModalOrderCancel, cancelOrder, getItemFlavorLines} = useOrderDetailController({ order, onClose });
+export default function OrderDetail({ order, onClose, updateStatusOrder }: OrderDetailProps) {
+  const { open, openModalOrderCancel, handleOpenModalOrderCancel, handleCloseModalOrderCancel, cancelOrder, getItemFlavorLines, imprimir } = useOrderDetailController({ order, onClose });
 
   if (!order) return null;
 
@@ -46,6 +41,17 @@ export default function OrderDetail({
 
   return (
     <>
+      <style>{`
+        .print-only { display: none; }
+        @media print {
+          body * { visibility: hidden; }
+          #print-order, #print-order * { visibility: visible; }
+          #print-order { display: block; position: absolute; top: 0; left: 0; width: 100%; }
+        }
+      `}</style>
+
+      <PrintOrder order={order} />
+
       <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
         <DialogTitle>
           <Stack direction="row" sx={{ alignItems: "center", justifyContent: "space-between" }}>
@@ -53,15 +59,7 @@ export default function OrderDetail({
               <Typography variant="body1">Pedido {order.code}</Typography>
             </Box>
           </Stack>
-          <IconButton
-            aria-label="Fechar"
-            onClick={onClose}
-            sx={{
-              position: "absolute",
-              right: 12,
-              top: 12,
-            }}
-          >
+          <IconButton aria-label="Fechar" onClick={onClose} sx={{ position: "absolute", right: 12, top: 12 }}>
             <CloseIcon />
           </IconButton>
         </DialogTitle>
@@ -69,10 +67,8 @@ export default function OrderDetail({
         <DialogContent dividers>
           <Grid container spacing={2}>
             <Grid size={{ xs: 12, md: 7 }}>
-              <Typography variant="subtitle1" gutterBottom>
-                Itens do pedido:
-              </Typography>
-              <Stack spacing={1.25} sx={{}}>
+              <Typography variant="subtitle1" gutterBottom>Itens do pedido:</Typography>
+              <Stack spacing={1.25}>
                 {order?.items?.map((item, i) => (
                   <Paper elevation={0} key={i} sx={{ p: 1 }}>
                     <Stack direction="row" sx={{ alignItems: "center", justifyContent: "space-between" }}>
@@ -100,107 +96,81 @@ export default function OrderDetail({
                   </Paper>
                 ))}
               </Stack>
-              <Typography variant="subtitle1" gutterBottom>
-                Observações:
-              </Typography>
-                  <Paper elevation={0}>
-                    <Typography variant="subtitle2" gutterBottom>
-                      {order.observation || "Nenhuma observação"}
-                    </Typography>
-                  </Paper>
+              <Typography variant="subtitle1" gutterBottom>Observações:</Typography>
+              <Paper elevation={0}>
+                <Typography variant="subtitle2" gutterBottom>
+                  {order.observation || "Nenhuma observação"}
+                </Typography>
+              </Paper>
             </Grid>
 
             <Grid size={{ xs: 12, md: 5 }}>
-              <Typography variant="subtitle1" gutterBottom>
-                Dados:
-              </Typography>
+              <Typography variant="subtitle1" gutterBottom>Dados:</Typography>
               <Stack spacing={2}>
                 <Paper elevation={0} sx={{ p: 1 }}>
                   <Divider sx={{ my: 1 }} />
-                  <Typography variant="subtitle2" gutterBottom>
-                    Cliente
-                  </Typography>
+                  <Typography variant="subtitle2" gutterBottom>Cliente</Typography>
                   <Typography>{order.costumerName}</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {order.costumerPhone}
-                  </Typography>
+                  <Typography variant="body2" color="text.secondary">{order.costumerPhone}</Typography>
                   <Divider sx={{ my: 1 }} />
 
                   {isDelivery ? (
                     <>
-                      <Typography variant="subtitle2" gutterBottom>
-                        Endereço
-                      </Typography>
+                      <Typography variant="subtitle2" gutterBottom>Endereço</Typography>
                       <Box>
-                        <Typography variant="body2" color="text.secondary">
-                          <strong>Bairro:</strong> {order.neighborhood?.neighborhoodName}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          <strong>Rua:</strong> {order.costumerAddress?.streetName}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          <strong>Número:</strong> {order.costumerAddress?.number}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          <strong>Cidade:</strong> {order.costumerAddress?.city}
-                        </Typography>
+                        <Typography variant="body2" color="text.secondary"><strong>Bairro:</strong> {order.neighborhood?.neighborhoodName}</Typography>
+                        <Typography variant="body2" color="text.secondary"><strong>Rua:</strong> {order.costumerAddress?.streetName}</Typography>
+                        <Typography variant="body2" color="text.secondary"><strong>Número:</strong> {order.costumerAddress?.number}</Typography>
+                        <Typography variant="body2" color="text.secondary"><strong>Cidade:</strong> {order.costumerAddress?.city}</Typography>
                       </Box>
                     </>
                   ) : (
                     <>
-                      <Typography variant="subtitle2" gutterBottom>
-                        Modalidade
-                      </Typography>
+                      <Typography variant="subtitle2" gutterBottom>Modalidade</Typography>
                       <Stack direction="row" spacing={1} sx={{ alignItems: "center" }}>
                         <StorefrontIcon fontSize="small" sx={{ color: "grey.500" }} />
-                        <Typography variant="body2" color="text.secondary">
-                          Retirada no local
-                        </Typography>
+                        <Typography variant="body2" color="text.secondary">Retirada no local</Typography>
                       </Stack>
                     </>
                   )}
                 </Paper>
                 <Divider sx={{ my: 1 }} />
                 <Paper elevation={0} sx={{ p: 1 }}>
-                  <Typography variant="subtitle2" gutterBottom>
-                    Pagamento
-                  </Typography>
-                  <Typography color="success" sx={{fontWeight: '600'}}>{paymentMethodMask(order.paymentMethod)}</Typography>
+                  <Typography variant="subtitle2" gutterBottom>Pagamento</Typography>
+                  <Typography color="success" sx={{ fontWeight: "600" }}>{paymentMethodMask(order.paymentMethod)}</Typography>
                 </Paper>
                 <Divider sx={{ my: 1 }} />
-              <Paper elevation={0} sx={{ p: 1 }}>
-                {isDelivery && (
+                <Paper elevation={0} sx={{ p: 1 }}>
+                  {isDelivery && (
+                    <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+                      <Typography variant="subtitle1">Taxa entrega</Typography>
+                      <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>{moneyMask(deliveryFee)}</Typography>
+                    </Stack>
+                  )}
                   <Stack direction="row" sx={{ justifyContent: "space-between" }}>
-                    <Typography variant="subtitle1">Taxa entrega</Typography>
-                    <Typography variant="subtitle1" sx={{fontWeight: '600'}}>{moneyMask(deliveryFee)}</Typography>
+                    <Typography variant="subtitle1">Subtotal</Typography>
+                    <Typography variant="h6" sx={{ fontWeight: "600" }}>{moneyMask(order.orderTotal)}</Typography>
                   </Stack>
-                )}
-                <Stack direction="row" sx={{ justifyContent: "space-between" }}>
-                  <Typography variant="subtitle1">Subtotal</Typography>
-                  <Typography variant="h6" sx={{fontWeight: '600'}}>{moneyMask(order.orderTotal)}</Typography>
-                </Stack>
-                <Stack direction="row" sx={{ justifyContent: "space-between" }}>
-                  <Typography variant="subtitle1">Total</Typography>
-                  <Typography variant="h6" sx={{fontWeight: '600'}}>{moneyMask(Number(order.orderTotal) + deliveryFee)}</Typography>
-                </Stack>
-              </Paper>
+                  <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+                    <Typography variant="subtitle1">Total</Typography>
+                    <Typography variant="h6" sx={{ fontWeight: "600" }}>{moneyMask(Number(order.orderTotal) + deliveryFee)}</Typography>
+                  </Stack>
+                </Paper>
               </Stack>
             </Grid>
           </Grid>
         </DialogContent>
 
         <DialogActions sx={{ justifyContent: "space-between" }}>
-          <Button sx={{textTransform: "none" }} variant="contained" color="error" size="medium"
-            onClick={() => handleOpenModalOrderCancel()}
-          >
+          <Button sx={{ textTransform: "none" }} variant="contained" color="error" size="medium" onClick={() => handleOpenModalOrderCancel()}>
             Cancelar
           </Button>
 
           <Stack direction="row" spacing={1}>
-            <Button sx={{textTransform: "none" }} variant="contained" color="success" size="medium" startIcon={<WhatsAppIcon />}>
+            <Button sx={{ textTransform: "none" }} variant="contained" color="success" size="medium" startIcon={<WhatsAppIcon />}>
               WhatsApp
             </Button>
-            <Button sx={{textTransform: "none" }} variant="contained" color="primary" size="medium" startIcon={<PrintIcon />}>
+            <Button onClick={() => imprimir()} sx={{ textTransform: "none" }} variant="contained" color="primary" size="medium" startIcon={<PrintIcon />}>
               Imprimir
             </Button>
             <Select
@@ -208,28 +178,13 @@ export default function OrderDetail({
               size="small"
               onChange={(e) => updateStatusOrder(e.target.value, order.id)}
               onClick={(e) => e.stopPropagation()}
-              sx={{
-                color: 'black',
-                backgroundColor: STATUS_COLOR(order.status),
-                '& .MuiSelect-icon': {
-                  color: 'black',
-                },
-                '& .MuiOutlinedInput-notchedOutline': {
-                  border: 'none',
-                },
-                
-                '&:hover .MuiOutlinedInput-notchedOutline': {
-                  border: 'none',
-                },
-              }}
+              sx={{ color: "black", backgroundColor: STATUS_COLOR(order.status), "& .MuiSelect-icon": { color: "black" }, "& .MuiOutlinedInput-notchedOutline": { border: "none" }, "&:hover .MuiOutlinedInput-notchedOutline": { border: "none" } }}
             >
               {Object.values(STATUS)
-              .filter((status) => status !== STATUS.CANCELADO)
-              .map((status) => (
-                <MenuItem key={status} value={status}>
-                  {STATUS_LABEL[status]}
-                </MenuItem>
-              ))}
+                .filter((status) => status !== STATUS.CANCELADO)
+                .map((status) => (
+                  <MenuItem key={status} value={status}>{STATUS_LABEL[status]}</MenuItem>
+                ))}
             </Select>
           </Stack>
         </DialogActions>
@@ -242,35 +197,21 @@ export default function OrderDetail({
               <Typography variant="body1">Cancelar pedido</Typography>
             </Box>
           </Stack>
-          <IconButton
-            aria-label="Fechar"
-            onClick={handleCloseModalOrderCancel}
-            sx={{
-              position: "absolute",
-              right: 12,
-              top: 12,
-            }}
-          >
+          <IconButton aria-label="Fechar" onClick={handleCloseModalOrderCancel} sx={{ position: "absolute", right: 12, top: 12 }}>
             <CloseIcon />
           </IconButton>
         </DialogTitle>
         <DialogContent>
           <Grid size={{ xs: 12, md: 7 }}>
-              <Typography variant="subtitle2" gutterBottom>
-                Essa ação não poderá ser desfeita!
-              </Typography>
-            </Grid>
+            <Typography variant="subtitle2" gutterBottom>Essa ação não poderá ser desfeita!</Typography>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Stack direction="row" spacing={1}>
-            <Button sx={{textTransform: "none" }} disableElevation variant="contained" color="inherit" size="medium" 
-              onClick={() => handleCloseModalOrderCancel()}
-            >
+            <Button sx={{ textTransform: "none" }} disableElevation variant="contained" color="inherit" size="medium" onClick={() => handleCloseModalOrderCancel()}>
               Voltar
             </Button>
-            <Button sx={{textTransform: "none" }} variant="contained" color="error" size="medium"
-              onClick={() => cancelOrder()}
-            >
+            <Button sx={{ textTransform: "none" }} variant="contained" color="error" size="medium" onClick={() => cancelOrder()}>
               Cancelar
             </Button>
           </Stack>
