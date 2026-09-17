deacc577d095be3f77f07d4ea00ec3171a873df8
widney <widneylima21@gmail.com>
2026-09-17 19:46:13 -0300

    Fix erro productSheet

--- src/components/ClientMenuComponents/ProductSheet/UseProductSheetController.tsx
+++ src/components/ClientMenuComponents/ProductSheet/UseProductSheetController.tsx
@@ -1,16 +1,22 @@
 import { useTheme } from "@mui/material/styles";
 import { useMediaQuery } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Additionals, Product, ProductCategory } from "../../../types/Product.type";
 import type { OrderItemBag } from "../../../types/Order.type";
 
-export default function UseProductSheetController(item: Product | undefined, category: ProductCategory, additionals: Additionals[]) {
+export default function UseProductSheetController(item: Product | undefined, category: ProductCategory, additionals: Additionals[]){
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   const [qty, setQty] = useState(item?.minQuantity ?? 1);
   const [observation, setObservation] = useState("");
   const [checkedAdditionals, setCheckedAdditionals] = useState<Additionals[]>([]);
   const ADDITIONALS = additionals ?? []
+
+  useEffect(() => {
+    setQty(item?.minQuantity ?? 1);
+    setObservation("");
+    setCheckedAdditionals([]);
+  }, [item?.id]);
 
   function additionalsByCategory() {
     return ADDITIONALS.filter((a) => a.category.id === category.id);
